f00085092f901e525a67178b543a40e85e161e77
Ben Askren <ben@buildcalc.com>
2021-03-05 07:19:02 -0500

    Initial Commit

---
+++ CrossPlatformApp/CrossPlatformApp.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace CrossPlatformApp.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new CrossPlatformApp.App(), args);
+            host.Run();
+        }
+    }
+}
